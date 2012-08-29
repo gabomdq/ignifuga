5e0585a9085f774e1e43c703322f01cd265de9b6
Gabriel Jacobo <gabomdq@gmail.com>
2012-08-28 22:26:17 -0300

    - Adds missing boost::python files - Adds License terms to Rocket glue files - Changes in .hgignore

--- src/backends/sdl/RocketGlueGLES.hpp
+++ src/backends/sdl/RocketGlueGLES.hpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2010-2012, Gabriel Jacobo
+ * All rights reserved.
+ * Permission to use this file is granted under the conditions of the Ignifuga Game Engine License
+ * whose terms are available in the LICENSE file or at http://www.ignifuga.org/license
+ *
+ */
+
 #ifndef SDLROCKETGLUEGLES_HPP
 #define SDLROCKETGLUEGLES_HPP
 
